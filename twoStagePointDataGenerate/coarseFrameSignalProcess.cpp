a8b03c966a27e5bd08864eb24da976b4b86b1711
zoe226 <wisdomh226@163.com>
2024-03-28 18:01:02 +0800

    add win function

--- twoStagePointDataGenerate/coarseFrameSignalProcess.cpp
+++ twoStagePointDataGenerate/coarseFrameSignalProcess.cpp
@@ -1,6 +1,10 @@
 #include "param.hpp"
 #include "coarseFrameSignalProcess.hpp"
 
+void func_winR_process(vector<vector<vector<float>>> &WinRout_RangeSampleNum_ChirpNum_RxNum, vector<uint8_t> &fft_win_type, unique_ptr<int16_t[]> &radarInputdata) {
+	// nowin,hann,hamm
+}
+
 void FFTD_SpatialFFT_CFAR_CoarseFrame(vector<vector<float>>& point_info, vector<vector<float>>& TOI, ParaSys& para_sys, Virtual_array& virtual_array, vector<complex<float>>& compensate_mat, vector<vector<vector<complex<float>>>>& CoarseRangeFFT_ValidCoarseRangeBinNum_ChirpNum_RxNum) {
 
 	// signal process
@@ -15,7 +19,8 @@ void func_signal_process_coarse(vector<vector<float>>& TOI, vector<vector<float>
 
 	if (para_sys.InputHasDonePreProcess == 0) {
 		// winr and fftr
-
+		vector<vector<vector<float>>> WinRout_RangeSampleNum_ChirpNum_RxNum(para_sys.RangeSampleNum, vector<vector<float>>(para_sys.VelocityNum, vector<float>(para_sys.RxNum)));
+		func_winR_process(WinRout_RangeSampleNum_ChirpNum_RxNum, para_sys.fft_win_type, radarInputdata);
 	}
 	else if (para_sys.InputHasDonePreProcess == 1) {
 		// reshape 
